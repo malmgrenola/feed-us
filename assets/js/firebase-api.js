cc6e3643c154bb73730bb97796773ca1ea598017
Ola Malmgren <malmgrenola@gmail.com>
2021-05-18 09:14:07 +0200

    (style): add firebase api, fingerprint generator & page render function

---
+++ assets/js/firebase-api.js
@@ -0,0 +1,43 @@
+console.log("fb api");
+
+function fbInit() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyCH9W1CsOl3TtjwIkJXjwpdx4yMuRVJJdA",
+    authDomain: "neon-research-304412.firebaseapp.com",
+    projectId: "neon-research-304412",
+    storageBucket: "neon-research-304412.appspot.com",
+    messagingSenderId: "693391729513",
+    appId: "1:693391729513:web:77200766e53d59756693f8",
+    measurementId: "G-RG1SVS1YH3"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
+
+function initDocRef(id) {
+  return firebase
+    .firestore()
+    .collection("users")
+    .doc(id);
+}
+
+function fbSignInAnonymously(id) {
+  return firebase.auth().signInAnonymously();
+}
+
+function fbGetUserDocument(id) {
+  return initDocRef(id).get();
+}
+
+function fbSetMealData(id, date, meal, currentMeals) {
+  const meals = currentMeals ? currentMeals : {};
+  meals[date] = meal;
+
+  currentMeals = meals;
+
+  return initDocRef(id).set({
+    fp: id,
+    meals: meals
+  });
+}
--- assets/js/meal-api.js
+++ assets/js/meal-api.js
@@ -6,6 +6,15 @@ function query() {
   ).then(
     function(response) {
       $("#meal-data").html(mealsResultHTML(response.meals));
+      $("#meal-data").on("click", ".qWeek li", function(event) {
+        console.log(
+          event.target.dataset.weekday,
+          event.target.dataset.mealid,
+          event.target.dataset
+        );
+        setMealData(event.target.dataset);
+      });
+      render();
     },
     function(errorResponse) {
       console.log(errorResponse);
@@ -21,12 +30,39 @@ function mealsResultHTML(meals) {
     return `No meals found`;
   }
   console.log(meals);
-  var mealItemsHTML = meals.map(function(meal) {
+  var mealItemsHTML = meals.map(function(meal, i) {
     //<a href="meal.html?id=${meal.idMeal}" >${meal.strMeal}</a>
+    function ingString(ingredient) {
+      const pm = meals.length - 1 !== i ? ", " : "";
+      return ingredient !== "" ? ingredient + pm : "";
+    }
+    let ingridients = [];
+
+    for (let i = 1; i <= 20; i++) {
+      if (meal[`strIngredient${i}`] !== "") {
+        ingridients.push(meal[`strIngredient${i}`]);
+      }
+    }
     return `<li>
-              ${meal.strMeal}
-            </li>`;
+      <img
+        src="${meal.strMealThumb}"
+        alt="${meal.strMeal} image"
+      />
+      <h4>${meal.strMeal}</h4>
+      <p>${ingridients.join(", ")}</p>
+      <ul class="qWeek">
+        <li data-weekday="Mon" data-mealid="${meal.idMeal}">Mon</li>
+        <li data-weekday="Tue" data-mealid="${meal.idMeal}">Tue</li>
+        <li data-weekday="Wed" data-mealid="${meal.idMeal}">Wed</li>
+        <li data-weekday="Thu" data-mealid="${meal.idMeal}">Thu</li>
+        <li data-weekday="Fri" data-mealid="${meal.idMeal}">Fri</li>
+        <li data-weekday="Sat" data-mealid="${meal.idMeal}">Sat</li>
+        <li data-weekday="Sun" data-mealid="${meal.idMeal}">Sun</li>
+        <li data-weekday="Fav" data-mealid="${meal.idMeal}">Fav</li>
+      </ul>
+    </li>`;
   });
+
   return `<div">
   <p><strong>${meals.length} meals found:</strong></p>
   <ul>
