a7e752928248d9a814294286855e0c5eddc24412
Ola Malmgren <malmgrenola@gmail.com>
2021-05-18 09:26:21 +0200

    (style): organize page renders

---
+++ assets/js/browse.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  console.log("document Ready");
+  render();
+});
+
+function render() {
+  console.log("render called");
+  if (session.data?.meals) {
+    const userMeals = session.data?.meals;
+
+    // Clear all states to default
+    $(`ul.qWeek li`).css("font-weight", "normal");
+
+    // Set states on page
+    for (const date in userMeals) {
+      $(
+        `ul.qWeek li[data-weekday='${date}'][data-mealid='${userMeals[date]}']`
+      ).css("font-weight", "bold");
+    }
+  }
+  console.log(1, {
+    fingerprint: session.id,
+    data: session.data,
+    user: session.user
+  });
+}
--- assets/js/feedUs.js
+++ assets/js/feedUs.js
@@ -4,28 +4,6 @@ $(document).ready(function() {
   console.log("document Ready");
 });
 
-function render() {
-  console.log("render called");
-  if (session.data?.meals) {
-    const userMeals = session.data?.meals;
-
-    // Clear all states to default
-    $(`ul.qWeek li`).css("font-weight", "normal");
-
-    // Set states on page
-    for (const date in userMeals) {
-      $(
-        `ul.qWeek li[data-weekday='${date}'][data-mealid='${userMeals[date]}']`
-      ).css("font-weight", "bold");
-    }
-  }
-  console.log(1, {
-    fingerprint: session.id,
-    data: session.data,
-    user: session.user
-  });
-}
-
 function initData() {
   console.log("initUserData - Loading");
   fbInit();
@@ -71,9 +49,10 @@ function initData() {
 }
 
 function setMealData(props) {
+  // Toggle meal
   const mealId =
     session.data?.meals[props.weekday] != props.mealid ? props.mealid : null;
-  console.log(2, mealId);
+
   fbSetMealData(session.id, props.weekday, mealId, session.data?.meals)
     .then(docs => {
       fbGetUserDocument(session.id)
