e665732e6907c449633a3ff5f832fcd3b7ba1d0d
Ola Malmgren <malmgrenola@gmail.com>
2021-06-05 22:58:55 +0200

    (style): clean up shopping list

--- assets/js/list.js
+++ assets/js/list.js
@@ -9,13 +9,20 @@ function render() {
 
 const listOnAddClick = () => {
   $("#addAdditionalItem").empty();
-  $("#addAdditionalItem").append("<input />");
+  $("#addAdditionalItem").append(
+    "<div class='bd-highlight'><i class='far fa-circle'></i></div><div class='flex-grow bd-highlight w-100 ms-2'><input /></div>"
+  );
   $("#addAdditionalItem").off();
   $("#addAdditionalItem input").focus();
   $("#addAdditionalItem input").keyup(event => {
     if (event.which == 13) {
       event.preventDefault();
       listAddAdditionalItem(event.target.value);
+      $("#btnAdd").focus();
+    }
+    if (event.which == 27) {
+      event.preventDefault();
+      render();
       $("#btnAdd").focus();
     }
   });
@@ -50,7 +57,12 @@ const listAddAdditionalItem = item => {
 
 const listDeleteAdditionalItem = index => {
   if (index >= 0) {
+    const value = `add-${session.data.additionalItems[index]}`;
+    const checkedIndex = session.data.shoppingChecked.findIndex(
+      item => item === value
+    );
     session.data.additionalItems.splice(index, 1);
+    if (checkedIndex >= 0) session.data.shoppingChecked.splice(checkedIndex, 1); //Clean up if item was among selected
   }
   fbSetDoc(session.id, session.data).catch(error => {
     console.error("Error writing document: ", error);
@@ -61,20 +73,17 @@ const listDeleteAdditionalItem = index => {
 const Page = () => {
   return `
   <div class="container-fluid py-2 grid">
-  <div class="row">
-    <div class="col text-center">
-      <h1>Feed Us!</h1>
+    <div class="row">
+      <div class="col text-center">
+        <h1>Feed Us!</h1>
+      </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="col text-center pb-1">
-      <h2>My Shopping list</h2>
+    <div class="row">
+      <div class="col text-center pb-1">
+        <h2>My Shopping list</h2>
+      </div>
     </div>
-  </div>
-    <div
-      id="cards"
-      class="row"
-    >${List()}</div>
+    <div class="row">${List()}</div>
   </div>`;
 };
 
@@ -138,88 +147,99 @@ const List = () => {
     return 0;
   });
 
+  const Ingridients = () => {
+    return ingridients
+      .map(({ name, selected, messure }, i) => {
+        const value = name;
+
+        return `
+        <li class="liCheck d-flex align-items-center bd-highlight">
+          <button
+          type="button"
+          class="btn btnCheck bd-highlight"
+          onclick="listToggleIngridient({idMeal: 'add', ingredient: '${name}',event: event})"
+          >${
+            selected
+              ? `<i class="far fa-check-circle"></i>`
+              : `<i class="far fa-circle"></i>`
+          }</button>
+          <div class="w-50 bd-highlight ms-2">
+            <button
+            type="button"
+            class="btn btnCheck text-left"
+            onclick="listToggleIngridient({idMeal: 'add', ingredient: '${name}',event: event})"
+            >${name}</button>
+          </div>
+          <div class="bd-highlight w-50 text-end">
+          ${messure.join(" + ")}
+          </div>
+        </li>`;
+      })
+      .join("\n");
+  };
+
+  const EmptyWeek = () => {
+    if (ingridients.length === 0) {
+      return `
+      <div class="col-12 p-2 text-center text-muted">
+        <p>When you select a dish on a weekday the ingredients will available for you here to use when you go shopping in the store.</p>
+        <p>You can hit <span class="a" onclick="listOnAddClick();">Add items</span> to add your extras to your shopping list!</p>
+      </div>`;
+    }
+    return "";
+  };
+
   return `
+  ${EmptyWeek()}
+  <div class="col-12 p-2 p-md-5">
+    <ul>
+      ${Ingridients()}
+      ${additionalItems
+        .map((item, i) => {
+          const value = `add-${item}`;
 
-  <div class="col-12 p-1">
-    <div class="card border-0">
-      <div class="card-body">
-        <ul>
-        ${ingridients
-          .map(({ name, selected, messure }, i) => {
-            const value = name;
+          const selected =
+            shoppingChecked.findIndex(item => item === value) >= 0
+              ? true
+              : false;
 
-            return `
-            <li class="liCheck d-flex bd-highlight">
+          return `
+          <li class="liCheck d-flex align-items-center bd-highlight">
+            <button
+            type="button"
+            class="btn btnCheck bd-highlight"
+            onclick="listToggleIngridient({idMeal: 'add', ingredient: '${value}',event: event})"
+            >${
+              selected
+                ? `<i class="far fa-check-circle"></i>`
+                : `<i class="far fa-circle"></i>`
+            }</button>
+            <div class="flex-grow-1 bd-highlight ms-2">
               <button
               type="button"
-              class="btn btnCheck bd-highlight"
-              onclick="listToggleIngridient({idMeal: 'add', ingredient: '${name}',event: event})"
-              >${
-                selected
-                  ? `<i class="far fa-check-circle"></i>`
-                  : `<i class="far fa-circle"></i>`
-              }</button>
-              <div class="flex-grow-1 bd-highlight">
-                <button
-                type="button"
-                class="btn btnCheck"
-                onclick="listToggleIngridient({idMeal: 'add', ingredient: '${name}',event: event})"
-                >${name}</button>
-              </div>
-              <div class="bd-highlight">
-              ${messure.join(" + ")}
-              </div>
-            </li>`;
-          })
-          .join("\n")}
-          ${additionalItems
-            .map((item, i) => {
-              const value = `add-${item}`;
-
-              const selected =
-                shoppingChecked.findIndex(item => item === value) >= 0
-                  ? true
-                  : false;
-
-              return `
-              <li class="liCheck d-flex bd-highlight">
-                <button
-                type="button"
-                class="btn btnCheck bd-highlight"
-                onclick="listToggleIngridient({idMeal: 'add', ingredient: '${value}',event: event})"
-                >${
-                  selected
-                    ? `<i class="far fa-check-circle"></i>`
-                    : `<i class="far fa-circle"></i>`
-                }</button>
-                <div class="flex-grow-1 bd-highlight">
-                  <button
-                  type="button"
-                  class="btn btnCheck"
-                  onclick="listToggleIngridient({idMeal: 'add', ingredient: '${value}',event: event})"
-                  >${item}</button>
-                </div>
-                <div class="bd-highlight">
-                  <button
-                  type="button"
-                  class="btn text-muted btnRemove"
-                  onclick="listDeleteAdditionalItem(${i})"
-                  ><i class="far fa-times-circle"></i></button>
-                </div>
-              </li>`;
-            })
-            .join("\n")}
-            <li><div id="addAdditionalItem">
-            <button
-            id="btnAdd"
-            type="button"
-            class="btn text-muted"
-            onclick="listOnAddClick()"
-            ><i class="fas fa-plus-circle" onclick="listOnAddClick();"></i> Add items</button>
-            </div> </li>
-        </ul>
-      </div>
-    </div>
+              class="btn btnCheck text-left"
+              onclick="listToggleIngridient({idMeal: 'add', ingredient: '${value}',event: event})"
+              >${item}</button>
+            </div>
+            <div class="bd-highlight">
+              <button
+              type="button"
+              class="btn text-muted btnRemove"
+              onclick="listDeleteAdditionalItem(${i})"
+              >Remove <i class="far fa-times-circle"></i></button>
+            </div>
+          </li>`;
+        })
+        .join("\n")}
+        <li class="liCheck d-flex bd-highlight"><div id="addAdditionalItem" class="d-flex bd-highlight w-100">
+        <button
+        id="btnAdd"
+        type="button"
+        class="btn text-muted"
+        onclick="listOnAddClick()"
+        ><i class="fas fa-plus-circle"></i> Add items</button>
+        </div> </li>
+    </ul>
   </div>
     `;
 };
